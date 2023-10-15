e72c34def57a314e53362ca6170110420c49ffdb
anthonydopeno <anthonydopeno@gmail.com>
2023-10-15 11:05:41 +0800

    web updates

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,7 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [ProductController::class, 'index'])->name('product.index');
 Route::get('/product', [ProductController::class, 'index'])->name('product.index');
 Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
 Route::post('/product', [ProductController::class, 'store'])->name('product.store');
